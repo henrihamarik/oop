2768c16122b906f7c1d220ec35ed3c9c180ea49f
henri <henri.hamarik@khk.ee>
2018-01-19 09:49:46 +0200

    programmi kasutamise juhis 1 sissejuhatava punktiga 2 kirjeldusega 3 projekti kaustad

---
+++ conf.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: henri
+ * Date: 19.01.2018
+ * Time: 09:45
+ */
+
+define('MODEL_DIR','model/');
+define('VIEW_DIR','views/');
+define('CONTROL_DIR','controllers/');
