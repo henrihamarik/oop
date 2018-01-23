1ba7b8c82a866393e2fc8af760bd72a4cdcdfe81
henri <henri.hamarik@khk.ee>
2018-01-23 12:22:23 +0200

    ok

--- model/template.php
+++ model/template.php
@@ -6,61 +6,88 @@
  * Time: 10:27
  */
 
+
 class template
 {
-    var $file = '';
-    var $content = false;
-    var $vars = array();
-
+    // klassi muutujad
+    var $file = ''; // HTML malli faili nimi
+    var $content = false; // HTML malli failist loetud sisu
+    var $vars = array(); // HTML malli elementide nimetuste ja reaalväärtuste paarid
+    /**
+     * template constructor.
+     * @param string $file
+     */
     public function __construct($file)
     {
-        $this->file = $file;
-        $this->loadFile();
+        $this->file = $file; // määrame kasutatava malli faili nimi
+        $this->loadFile(); // laadime määratud nimega faili sisu
     }
-
+    // HTML malli faili nime ja õiguste kontroll
+    // ning sisu lugemine siis, kui vajalikud
+    // tingimused on täidetud
     function loadFile(){
-        if (!is_dir(VIEW_DIR)){
-            echo 'Ei ole leitud '.VIEW_DIR.' kataloogi <br />';
+        if(!is_dir(VIEW_DIR)){
+            echo 'Ei ole leitud '.VIEW_DIR.' kataloogi<br />';
             exit;
         }
-        $file = $this->file;
-        if (file_exists($file) and is_file($file) and is_readable($file)){
+        // kui faili nimi on määratud kujul
+        // views/failinimi.html
+        $file = $this->file; // abiasendus
+        if(file_exists($file) and is_file($file) and is_readable($file)){
             $this->readFile($file);
         }
-        $file = VIEW_DIR.$this->file.'.html';
-        if (file_exists($file) and is_file($file) and is_readable($file)){
+        // kui faili nimi on määratud kujul
+        // failinimi.html
+        $file = VIEW_DIR.$this->file; // abiasendus
+        if(file_exists($file) and is_file($file) and is_readable($file)){
             $this->readFile($file);
         }
-        $file = VIEW_DIR.str_replace('.', '/', $this->file).'.html';
-        if (file_exists($file) and is_file($file) and is_readable($file)){
+        // kui faili nimi on määratud kujul
+        // failinimi
+        $file = VIEW_DIR.$this->file.'.html'; // abiasendus
+        if(file_exists($file) and is_file($file) and is_readable($file)){
             $this->readFile($file);
         }
-        if ($this->content === false){
-            echo 'Ei suutnud lugeda'.$this->file.' sisu<br />';
+        // kui fail asub alamkataloogis views/alamkaust/failinimi.html
+        // kui faili nimi on määratud kujul
+        // alamkaust.failinimi
+        $file = VIEW_DIR.str_replace('.', '/', $this->file).'.html'; // abiasendus
+        if(file_exists($file) and is_file($file) and is_readable($file)){
+            $this->readFile($file);
+        }
+        // kui ikkagi sisu on puudu
+        if($this->content === false){
+            echo 'Ei suutnud lugeda '.$this->file.' sisu <br />';
             exit;
         }
     }
-
+    // HTML malli failist sisu lugemine
     function readFile($file){
-/*        $fp = fopen($file, 'r');
+        /*$fp = fopen($file, 'r');
         $this->content = fread($fp, filesize($file));
-        fclose($fb)*/
+        fclose($fp);*/
         $this->content = file_get_contents($file);
     }
+    // malli elemendi nimi ja reaalväärtuse paari koostamine
+    // ja lisamine $this->vars massiivi sisse
     function set($name, $value){
         $this->vars[$name] = $value;
     }
+    // malli elemendi nimi ja reaalväärtuse paari täiendamine
+    // $this->vars massiivis
     function add($name, $value){
         if(!isset($this->vars[$name])){
             $this->set($name, $value);
         } else {
-            $this->vars[$name] = $this->vars[$name] . $value;
+            $this->vars[$name] = $this->vars[$name].$value;
         }
     }
-    function  parse() {
-        $str = $this->content;
+    // täidame mallist loetud sisu reaalsete väärtustega ja
+    // anname muudetud sisu tagasi põhiprogrammile
+    function parse(){
+        $str = $this->content; // malli sisu algväärtus
         foreach ($this->vars as $name=>$value){
-            $str=str_replace('{'.$name.'}', $value, $str);
+            $str = str_replace('{'.$name.'}', $value, $str);
         }
         return $str;
     }
