d36784c0499e269d991cb056c69fb2be99491bcc
henri <henri.hamarik@khk.ee>
2018-01-23 12:30:49 +0200

    http ja serveri andmete töötlusklass

---
+++ model/http.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: henri
+ * Date: 23.01.2018
+ * Time: 12:22
+ */
+
+class http
+{
+    var $vars = array();
+    var $server = array();
+    public function __construct()
+    {
+        $this->init();
+    }
+
+    function init(){
+        $this->vars = array_merge($_GET, $_POST;
+        $this->server = $_SERVER;
+    }
+}
