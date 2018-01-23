ff0a5be6c01628939b996071fcd78b59612ac84a
henri <henri.hamarik@khk.ee>
2018-01-23 12:48:23 +0200

    ok

--- model/http.php
+++ model/http.php
@@ -13,10 +13,20 @@ class http
     public function __construct()
     {
         $this->init();
+        $this->initConst();
     }
 
     function init(){
         $this->vars = array_merge($_GET, $_POST);
         $this->server = $_SERVER;
+    }
+
+    function initConst(){
+        $constNames = array('HTTP_HOST','SCRIPT_NAME','REMOTE_ADDR');
+        foreach ($constNames as $constName){
+            if(!define($constName)and isset($this->server[$constName])){
+                define($constName, $this->server[$constName]);
+            }
+        }
     }
 }
