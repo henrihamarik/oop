4e693485cc8f69ae5b3649ca880a95027c0b96fe
henri <henri.hamarik@khk.ee>
2018-01-23 11:13:55 +0200

    ok

--- menu.php
+++ menu.php
@@ -7,3 +7,8 @@
  */
 $menuTmpl = new templates('menu.menu');
 $menuItemTmpl = new templates('menu.menu_item');
+$menuItemTmpl->set('menu_item_name','esimene');
+echo '<pre>';
+print_r($menuItemTmpl);
+echo '</pre>';
+echo $menuItemTmpl->parse();
