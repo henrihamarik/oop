9824dc18f08b538647fc83a4eab5b207376f6ff4
henri <henri.hamarik@khk.ee>
2018-04-05 22:56:52 +0300

    ok

--- model/session.php
+++ model/session.php
@@ -36,23 +36,23 @@ class session
                 'role_id' => 0,
                 'username' => 'Anonüümne'
             );
-            // loome sid
-            $sid = md5(uniqid(time().mt_rand(1,1000), true));
-            // salvestame andmed session tabelisse
-            $sql = 'INSERT INTO session SET '.
-                'sid='.fixDb($sid).', '.
-                'user_id='.fixDb($user['user_id']).', '.
-                'user_data='.fixDb(serialize($user)).', '.
-                'login_ip='.fixDb(REMOTE_ADDR).', '.
-                'created=NOW()';
-            // saadame päring andmebaasi
-            $this->db->query($sql);
-            // määrame sid $this->sid
-            $this->sid = $sid;
-            // lisame andmed $http objekti sisse
-            // et nad oleks veeebis kätte saadavad
-            $this->http->set('sid', $sid);
-        }
+        }// oli reas 57
+        //loome sid
+        $sid = md5(uniqid(time().mt_rand(1,1000), true));
+        // salvestame andmed session tabelisse
+        $sql = 'INSERT INTO session SET '.
+            'sid='.fixDb($sid).', '.
+            'user_id='.fixDb($user['user_id']).', '.
+            'user_data='.fixDb(serialize($user)).', '.
+            'login_ip='.fixDb(REMOTE_ADDR).', '.
+            'created=NOW()';
+        // saadame päring andmebaasi
+        $this->db->query($sql);
+        // määrame sid $this->sid
+        $this->sid = $sid;
+        // lisame andmed $http objekti sisse
+        // et nad oleks veeebis kätte saadavad
+        $this->http->set('sid', $sid);
     }
     // lõpenute sessioonide kustutamine andmebaasist
     function clearSessions(){
@@ -110,4 +110,5 @@ class session
             define('ROLE_ID', 0);
         }
     }
+
 }
